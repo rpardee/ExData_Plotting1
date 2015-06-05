7cdcf59211292cbda1dca1a21989648de17fe684
Roy Pardee <pardee.r@ghc.org>
2015-06-04 20:15:23 -0700

    First pass at least.  Maybe done.

--- make_plots.R
+++ make_plots.R
@@ -6,6 +6,43 @@ hpc <- read.table(file = 'data/household_power_consumption.txt'
               , na.strings = "?")
 #               , skip = 21997 <-- kills the header read?
 
-# hpc <- subset(hpc, Date %in% c('1/2/2007', '2/2/2007'))
-hpc <- hpc[hpc$Date %in% c('1/2/2007', '2/2/2007'),]
+hpc <- subset(hpc, Date %in% c('1/2/2007', '2/2/2007'))
 # hpc$Date <- as.Date(strptime(hpc$Date, format = '%d/%m/%Y'))
+hpc$dt <- as.POSIXct(strptime(paste(hpc$Date, hpc$Time), format = '%d/%m/%Y %H:%M:%S'))
+png(file = "plot1.png")
+hist(hpc$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.off()
+
+png(file = "plot2.png")
+plot(hpc$dt, hpc$Global_active_power, type="n", main = "", xlab = "", ylab = "Global Active Power")
+lines(hpc$dt, hpc$Global_active_power)
+dev.off()
+
+png(file = "plot3.png")
+with(hpc, plot(dt, Sub_metering_1, type="n", xlab = "", ylab = "Energy sub metering"))
+with(hpc, lines(dt, Sub_metering_1, col = "black"))
+with(hpc, lines(dt, Sub_metering_2, col = "red"))
+with(hpc, lines(dt, Sub_metering_3, col = "blue"))
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd = 2, col = c("black", "red", "blue"))
+dev.off()
+
+png(file = "plot4.png")
+par(mfcol = c(2, 2))
+
+plot(hpc$dt, hpc$Global_active_power, type="n", main = "", xlab = "", ylab = "Global Active Power")
+lines(hpc$dt, hpc$Global_active_power)
+
+with(hpc, plot(dt, Sub_metering_1, type="n", xlab = "", ylab = "Energy sub metering"))
+with(hpc, lines(dt, Sub_metering_1, col = "black"))
+with(hpc, lines(dt, Sub_metering_2, col = "red"))
+with(hpc, lines(dt, Sub_metering_3, col = "blue"))
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd = 2, col = c("black", "red", "blue"), bty="n")
+
+with(hpc, plot(dt, Voltage, type="n", xlab = "datetime"))
+with(hpc, lines(dt, Voltage))
+
+with(hpc, plot(dt, Global_reactive_power, type="n", xlab = "datetime"))
+with(hpc, lines(dt, Global_reactive_power))
+
+dev.off()
+
